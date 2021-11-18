75db7701ac034e7a4f761054ff81e1b17e4c1310
Kuckelkorn <remco.kuckelkorn@soverin.net>
2021-11-18 14:26:10 +0100

    add class to div

--- src/scripts/helpers/render.js
+++ src/scripts/helpers/render.js
@@ -3,7 +3,7 @@ import findTaste from "../findTaste.js";
 function createCard(obj) {
   const element = document.createElement("div")
   element.addEventListener("click", () => findTaste(obj.id))
-  element.class= "dish card"
+  element.classList.add("card")
   element.id = obj.id
   element.innerHTML = `<p> ${obj.title} </p> <img src=${obj.image}>`
   document.body.appendChild(element)
