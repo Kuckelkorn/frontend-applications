1079c3e44d43d36f2d8757936eb6bcd308c1e6cd
Kuckelkorn <remco.kuckelkorn@soverin.net>
2021-11-22 19:47:05 +0100

    working barchart

--- src/components/Chart.svelte
+++ src/components/Chart.svelte
@@ -1,11 +1,25 @@
 <script>
   import * as d3 from 'd3';
   import {TasteStore} from '../stores/tasteStore'
-  import Recipe from "./Recipe.svelte"
+
+  const margin = {top: 40, bottom: 10, left:120, right: 20}
+  const width = 800 - margin.left - margin.right
+  const height = 600 - margin.top - margin.bottom
+
+
+  $: xScale = d3.scaleLinear().range([0, width]).domain([0, d3.max($TasteStore, (d) => d.value)])
+  $: yScale = d3.scaleBand().range([0, height]).domain($TasteStore.map((d) => d.taste)).padding(0.1)
 </script>
 
-{#each $TasteStore as data}
-  <svg>
-
-  </svg>
-{/each}
+<svg {width} {height}>
+  {#each $TasteStore as data}
+    <g>
+      <rect
+        x={0}
+        y={yScale(data.taste)}
+        width={xScale(data.value)}
+        height={yScale.bandwidth()}
+      />
+    </g>
+  {/each}
+</svg>
